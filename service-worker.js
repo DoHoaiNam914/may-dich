984991be4784b16cafcb9d1334a1f788e9206ed3
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-08-23 16:24:07 +0700

    Update

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+'use strict'
+/* global addEventListener, caches, clients, fetch, skipWaiting */
+const cacheName = 'Dịch thuật'
+const precachedResources = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/icons.svg',
+  '/manifest.json'
+]
+async function precache () {
+  const cache = await caches.open(cacheName)
+  return await cache.addAll(precachedResources)
+}
+addEventListener('install', event => {
+  event.waitUntil(precache())
+  skipWaiting()
+})
+addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(value => Promise.all(value.map(element => {
+    if (element === cacheName) return null
+    return caches.delete(element)
+  }))))
+  clients.claim()
+})
+async function networkFirst (request) {
+  try {
+    const networkResponse = await fetch(request)
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName)
+      cache.put(request, networkResponse.clone())
+    }
+    return networkResponse
+  } catch {
+    const cachedResponse = await caches.match(request)
+    if (cachedResponse != null) return cachedResponse
+    if (request.mode === 'navigate') return await caches.match('/index.html')
+    return Response.error()
+  }
+}
+addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(networkFirst(event.request))
+})
