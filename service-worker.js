9996ffcd6fc9e6303f36cd30faf03fdd673844c8
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-08-27 19:10:46 +0700

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict'
 /* global addEventListener, caches, clients, fetch, skipWaiting */
-const cacheName = '26.08.24-alpha'
+const cacheName = '26.08.27-alpha'
 const precachedResources = [
   '/',
   '/index.html',
