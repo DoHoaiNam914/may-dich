24078e5118b2ec163ada263f96029d5c0ef876f2
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-09-03 23:37:13 +0700

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,10 @@
 'use strict'
-/* global addEventListener, caches, clients, fetch, skipWaiting */
-const cacheName = '26.08.27-alpha'
-const precachedResources = [
-  '/',
-  '/index.html',
-  '/favicon.svg',
-  '/icons.svg',
-  '/manifest.json'
-]
-async function precache () {
-  const cache = await caches.open(cacheName)
-  return await cache.addAll(precachedResources)
-}
-addEventListener('install', event => {
-  event.waitUntil(precache())
-  skipWaiting()
-})
+/* global addEventListener, caches */
+const cacheName = '26.09.03-alpha'
 addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(value => Promise.all(value.map(element => {
     if (element === cacheName) return null
     return caches.delete(element)
   }))))
   clients.claim()
-})
-async function networkFirst (request) {
-  try {
-    const networkResponse = await fetch(request)
-    if (networkResponse.ok) {
-      const cache = await caches.open(cacheName)
-      cache.put(request, networkResponse.clone())
-    }
-    return networkResponse
-  } catch {
-    const cachedResponse = await caches.match(request)
-    if (cachedResponse != null) return cachedResponse
-    if (request.mode === 'navigate') return await caches.match('/index.html')
-    return Response.error()
-  }
-}
-addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-  event.respondWith(networkFirst(event.request))
 })
